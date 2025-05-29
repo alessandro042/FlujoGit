ac812f4dc861af0ee18559f95d6396957ec7e96b
alessandro042 <20223tn058@utez.edu.mx>
2025-05-28 18:07:52 -0600

    [FIRST COMMIT SUBIDA INICIAL DEL PROYECTO]

---
+++ src/main/java/utez/edu/mx/flujogit/FlujoGitApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.flujogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlujoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlujoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/flujogit/FlujoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.flujogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlujoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
